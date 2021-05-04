feb3dd533272b4dc45ee41d1ce9a43e8488296f8
Andrea Leardini <andre8244@gmail.com>
2021-05-04 16:00:46 +0200

    carbon, header and grid

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="bx--grid bx--grid--full-width">
+    <div class="bx--row">
+      <div class="bx--col-lg-16">full width column</div>
+    </div>
+    <div class="bx--row">
+      <div class="bx--col-md-4 bx--col-lg-7">7/16</div>
+      <div class="bx--col-md-4 bx--offset-lg-1 bx--col-lg-8">8/16</div>
+    </div>
+    <div class="bx--row">
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+      <div class="bx--col-md-4 bx--col-lg-4">1/4</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Dashboard",
+  components: {},
+};
+</script>
